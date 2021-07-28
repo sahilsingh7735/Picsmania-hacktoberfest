b55873b139b7eb303d46b54afc39bbf76672b349
sahilsingh7735 <sahilsingh7735@gmail.com>
2021-07-28 22:59:27 +0530

    updating file

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBMl-pVKze91ga09u9lIGmWrUm9-PJYN6g",
-    authDomain: "instagram-clone-react-f196e.firebaseapp.com",
-    projectId: "instagram-clone-react-f196e",
-    storageBucket: "instagram-clone-react-f196e.appspot.com",
-    messagingSenderId: "675741048996",
-    appId: "1:675741048996:web:d890446ebde1b62b1bc631",
-    measurementId: "G-Z4XTY9R9E6"
+  apiKey: "AIzaSyAtIFgL2n1GGaSjJCXpJ14pu2amMTd6hkM",
+  authDomain: "picsmania-c099f.firebaseapp.com",
+  projectId: "picsmania-c099f",
+  storageBucket: "picsmania-c099f.appspot.com",
+  messagingSenderId: "452053930738",
+  appId: "1:452053930738:web:107acfa59d61bc4bd7b66b",
+  measurementId: "G-79Y8S124GQ"
 });
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
